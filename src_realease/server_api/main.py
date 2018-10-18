5209d5d79a63c735aef0c84b1f93b4005ec9fd08
pedro <porellana@tecnosmart.cl>
2018-10-18 11:33:46 -0300

    api

--- src_realease/server_api/main.py
+++ src_realease/server_api/main.py
@@ -1,8 +1,29 @@
-from flask import Flask
+from flask import Flask 
+from flask_restful import Resource, Api, reqparse
+
+
 
 app = Flask(__name__)
+api = Api(app)
+parser = reqparse.RequestParser()
 
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+class root(Resource):
+    def get(self):
+        return {'quote': ['api clasificadora de noticias'] }
+    
+    def post(self):
+        """ recive tipo x-www-form-urlencoded """
+        args = parser.parse_args() # json de request
+        parser.add_argument('cuerpo', type=str)
+
+        return {
+            'status': True,
+            'cuerpo': '{} added. Good'.format(args['cuerpo'])
+        }
+
+
+api.add_resource(root, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
